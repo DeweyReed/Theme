810186411705fb0d47dc36e9efe1c072c9b9091b
Reed <deweyreed@protonmail.com>
2019-04-15 20:09:46 +0800

    Tint FAB with material colors

--- theme/src/main/java/xyz/aprildown/theme/views/material/ThemeFloatingActionButton.kt
+++ theme/src/main/java/xyz/aprildown/theme/views/material/ThemeFloatingActionButton.kt
@@ -1,18 +1,14 @@
 package xyz.aprildown.theme.views.material
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
-import android.graphics.Color.BLACK
-import android.graphics.Color.WHITE
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import xyz.aprildown.theme.R
 import xyz.aprildown.theme.Theme
 import xyz.aprildown.theme.internal.AttrWizard
 import xyz.aprildown.theme.utils.ColorUtils
-import xyz.aprildown.theme.utils.color
 import xyz.aprildown.theme.utils.colorForAttrName
 import xyz.aprildown.theme.utils.tint
 
@@ -23,48 +19,21 @@ internal class ThemeFloatingActionButton(
 
     private val wizard = AttrWizard(context, attrs)
     private val backgroundColorValue = wizard.getRawValue(android.R.attr.background)
-    private var iconColor: Int = 0
+    private var colorOnFab: Int = 0
 
     init {
         val theme = Theme.get()
         theme.colorForAttrName(backgroundColorValue, theme.colorAccent)?.let {
-            invalidateColors(it)
+            colorOnFab = if (ColorUtils.isLightColor(it)) Color.BLACK else Color.WHITE
+            setImageDrawable(drawable)
+
+            backgroundTintList = ColorStateList.valueOf(it)
+            rippleColor = colorOnFab
         }
     }
 
-    override fun setImageDrawable(drawable: Drawable?) =
-        super.setImageDrawable(drawable?.tint(iconColor))
-
-    private fun invalidateColors(color: Int) {
-        setTintSelector(color)
-        iconColor = if (ColorUtils.isLightColor(color)) BLACK else WHITE
-        setImageDrawable(drawable)
+    override fun setImageDrawable(drawable: Drawable?) {
+        super.setImageDrawable(drawable?.tint(colorOnFab))
     }
 
-    @SuppressLint("PrivateResource")
-    private fun setTintSelector(color: Int) {
-        val isColorLight = ColorUtils.isLightColor(color)
-        val rippleColor = context.color(
-            if (isColorLight) R.color.ripple_material_light
-            else R.color.ripple_material_dark
-        )
-        val pressed = ColorUtils.shiftColor(color, 1.1f)
-        val textColor = context.color(
-            if (isColorLight) R.color.theme_primary_text_light
-            else R.color.theme_primary_text_dark
-        )
-        // FloatingActionButton doesn't support disabled state?
-        val sl = ColorStateList(
-            arrayOf(
-                intArrayOf(-android.R.attr.state_pressed), intArrayOf(android.R.attr.state_pressed)
-            ),
-            intArrayOf(color, pressed)
-        )
-
-        this.rippleColor = rippleColor
-        this.backgroundTintList = sl
-        if (this.drawable != null) {
-            setImageDrawable(drawable.tint(textColor))
-        }
-    }
 }
