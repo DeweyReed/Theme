e7fd2d9a378210c6ad607ca6019bb904bdc92570
Reed <deweyreed@protonmail.com>
2019-08-18 20:58:23 +0800

    Clean up dependencies

--- app/src/main/java/xyz/aprildown/theme/app/App.kt
+++ app/src/main/java/xyz/aprildown/theme/app/App.kt
@@ -3,16 +3,11 @@
 package xyz.aprildown.theme.app
 
 import android.app.Application
-import com.squareup.leakcanary.LeakCanary
 import xyz.aprildown.theme.Theme
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
-        }
-        LeakCanary.install(this)
 
         Theme.init(this) {
             colorPrimaryRes = R.color.colorPrimary
