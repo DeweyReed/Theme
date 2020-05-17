45108308f0f15a5f3f94ae59996d64f5e1d443c9
Reed <deweyreed@protonmail.com>
2020-05-17 19:18:44 +0800

    Make VersionHelper's methods internal

--- theme/src/main/java/xyz/aprildown/theme/utils/VersionHelper.kt
+++ theme/src/main/java/xyz/aprildown/theme/utils/VersionHelper.kt
@@ -2,14 +2,14 @@ package xyz.aprildown.theme.utils
 
 import android.os.Build
 
-fun isMOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+internal fun isMOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 
-// fun isNOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+// internal fun isNOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 
-// fun isNMR1OrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+// internal fun isNMR1OrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
 
-fun isOOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+internal fun isOOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
-fun isPOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+internal fun isPOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
 
-fun isQOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+internal fun isQOrLater(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
